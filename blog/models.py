693367588cdd596f28913f757793d7a4c08d403e
Ben Lardie <benlardie@gmail.com>
2019-03-04 14:36:45 -0500

    articles are working

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,5 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     draft = models.BooleanField()
-    published_date = datetime.DateField.auto_now
-    author = models.CharField()
+    published_date = models.DateField()
+    author = models.CharField(max_length=255)
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,13 @@
 
 from django.shortcuts import render
-import datetime
 from django.http import HttpResponse, HttpResponseRedirect
+from blog.models import Article
 
 
 def root(request):
     return HttpResponseRedirect('home')
 
 def home_page(request):
-    now = str(datetime.datetime.now())
-    context = {'day': now}
+    context = {'articles': Article.objects.all()}
     response = render(request, 'index.html', context)
     return HttpResponse(response)
